fc4ca8b413becbd45be812b9c9f85236398f9ae3
Bri@n <bdonaldson@pm.me>
2020-10-15 23:15:07 -0400

    Added Servuce

---
+++ FrasasoSrvInstaller.hpp
@@ -0,0 +1,15 @@
+#ifndef FRASASOSRV_INSTALLER_HPP
+#define FRASASOSRV_INSTALLER_HPP
+
+#include "FrasasoSrvBase.hpp"
+
+class FrasasoSrvInstaller {
+public:
+  static bool Install(const FrasasoSrvBase& service);
+  static bool Uninstall(const FrasasoSrvBase& service);
+private:
+	FrasasoSrvInstaller() {}
+};
+
+#endif // FRASASOSRV_INSTALLER_HPP
+
---
+++ README.md
@@ -0,0 +1,8 @@
+Windows service base
+===============
+
+Base for writing Windows services in C++
+
+Contains an example of a service that track user log ons/offs and locks/unlocks.
+Also serves as an example of getting user name under when the process is running under
+System account.
